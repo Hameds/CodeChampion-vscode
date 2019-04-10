c845211cc01258ca3753b8f2d4484b5b7b0c8d22
mehdy <m.m.mohammadi99@gmail.com>
2019-04-10 23:16:10 +0430

    - (Refactor) change is win if to ternary-OP

--- src/extension.ts
+++ src/extension.ts
@@ -14,8 +14,7 @@ export function activate(context: vscode.ExtensionContext) {
     'Congratulations, your extension "codechampion-vscode" is now active!'
   );
   function playSound(winOrFail: string) {
-    var isWin = false;
-    if (winOrFail === 'win') isWin = !isWin;
+    var isWin = (winOrFail === 'win') ? true : false;
 
     var configs = vscode.workspace
     .getConfiguration();
